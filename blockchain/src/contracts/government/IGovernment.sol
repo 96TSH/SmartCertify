7818ac390d508e39f669baa37344ca79d466d935
zhougaoqiang <45629779+zhougaoqiang@users.noreply.github.com>
2024-03-29 13:23:43 +0800

    remove all

--- blockchain/src/contracts/government/IGovernment.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IGovernment//this contract address will publish to all
-{
-    function isRegisterSchool(address _add) external view returns (bool);
-    function isRegisterCompany(address _add) external view returns (bool);
-    function isRegisterPerson(address _add) external view returns (bool);
-}
