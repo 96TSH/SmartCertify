1e8eb3e847097e66878671d4ec33cda79a86b791
96TSH <tansiheng96@gmail.com>
2024-04-04 11:04:14 +0800

    updated useNavigate

--- web-app/src/components/Login.jsx
+++ web-app/src/components/Login.jsx
@@ -1,4 +1,5 @@
-import { useState, useContext, useNavigate } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   TextField,
   Button,
